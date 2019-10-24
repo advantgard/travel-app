2b816982245932203f0d3c2ca285153897c2dd5f
Jorge Alvarez <j.alvarz@hotmail.com>
2019-10-24 15:15:45 -0700

    Initial setup

---
+++ services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBy0vm8D-sxttWkryDAHrO7K1KgWxYv_Io",
+    authDomain: "travel-planner-e252f.firebaseapp.com",
+    databaseURL: "https://travel-planner-e252f.firebaseio.com",
+    projectId: "travel-planner-e252f",
+    storageBucket: "travel-planner-e252f.appspot.com",
+    messagingSenderId: "732037472555",
+    appId: "1:732037472555:web:c7c658aee75003547ba56e",
+    measurementId: "G-N5JHW91CBX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
