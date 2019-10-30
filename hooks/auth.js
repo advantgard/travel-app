e9791667eeb39649616d64dda320f34624167452
Jorge Alvarez <j.alvarz@hotmail.com>
2019-10-29 17:36:32 -0700

    Reformat with prettier

--- hooks/auth.js
+++ hooks/auth.js
@@ -3,69 +3,69 @@ import { useState, useEffect } from "react";
 import firebase from "../services/firebase";
 
 export function signIn(provider) {
-    let authProvider = {};
+  let authProvider = {};
 
-    switch (provider) {
-        case "google":
-            authProvider = new firebase.auth.GoogleAuthProvider();
-            break;
+  switch (provider) {
+    case "google":
+      authProvider = new firebase.auth.GoogleAuthProvider();
+      break;
 
-        case "facebook":
-            break;
+    case "facebook":
+      break;
 
-        default:
-            // Default is email sign in
-            break;
-    }
+    default:
+      // Default is email sign in
+      break;
+  }
 
-    firebase
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function() {
+      firebase
         .auth()
-        .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function() {
-            firebase
-                .auth()
-                .signInWithPopup(authProvider)
-                .then(credentials => {
-                    firebase
-                        .firestore()
-                        .collection("users")
-                        .doc(credentials.user.uid)
-                        .set({
-                            email: credentials.user.email
-                        });
-                })
-                .catch(function(error) {
-                    console.error(error.message);
-                });
+        .signInWithPopup(authProvider)
+        .then(credentials => {
+          firebase
+            .firestore()
+            .collection("users")
+            .doc(credentials.user.uid)
+            .set({
+              email: credentials.user.email
+            });
+        })
+        .catch(function(error) {
+          console.error(error.message);
         });
+    });
 }
 
 export function signOut() {
-    firebase
-        .auth()
-        .signOut()
-        .then(
-            () => {
-                console.log("Sign out successful");
-            },
-            error => {
-                console.log(error.message);
-            }
-        );
+  firebase
+    .auth()
+    .signOut()
+    .then(
+      () => {
+        console.log("Sign out successful");
+      },
+      error => {
+        console.log(error.message);
+      }
+    );
 }
 
 export function useUser() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        firebase.auth().onAuthStateChanged(userData => {
-            if (userData) {
-                setUser(userData);
-            } else {
-                setUser(null);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(userData => {
+      if (userData) {
+        setUser(userData);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
 
-    return user;
+  return user;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
 import React from "react";
 import Head from "next/head";
+import { useUser } from "../hooks/auth";
 import { Nav } from "../components/nav";
-import { TripList } from "../components/trip";
+import { TripList, TripNew } from "../components/trip";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossOrigin="anonymous"
-      />
-      <link rel="stylesheet" href="/styles/react-datepicker.min.css" />
-    </Head>
+const Home = () => {
+  const user = useUser();
 
-    <Nav />
-    <div className="container">
-      <TripList />
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/styles/react-datepicker.min.css" />
+      </Head>
+
+      <Nav />
+      <div className="container">
+        <div className="row">
+            <div className="col-sm-2">
+                Friend Section
+            </div>
+            <div className="col-sm-10">
+                <TripList />
+                { user ? <TripNew/> : "" }
+            </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
