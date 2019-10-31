8e7e23cb1a17fae622994f85a2fcf18724e9fc5a
Jorge Alvarez <j.alvarz@hotmail.com>
2019-10-30 18:29:35 -0700

    Add airport autocomplete interface

--- components/airport.js
+++ components/airport.js
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import { airports } from "../data/airports";
 import Fuse from "fuse.js";
 
-export const AirportSelect = () => {
+export const AirportSelect = ({
+  autocompleteOnLength = 3,
+  limitSuggestions = 10
+}) => {
   const [searchText, changeSearchText] = useState("");
+  const [suggestions, changeSuggestions] = useState([]);
+  const [selected, changeSelected] = useState(null);
 
   const fuseOptions = {
     shouldSort: true,
@@ -29,8 +34,36 @@ export const AirportSelect = () => {
 
   const doSearch = text => {
     changeSearchText(text);
-    console.log(fuse.search(text));
+
+    if (text.length >= autocompleteOnLength) {
+      changeSuggestions(fuse.search(text).slice(0, limitSuggestions));
+    } else {
+      changeSuggestions([]);
+    }
   };
+
+  const selectItem = airport => {
+    changeSelected(airport);
+    changeSearchText("");
+    changeSuggestions([]);
+  };
+
+  const SuggestionList = () => (
+    <ul className="list-group">
+      {suggestions.map((airport, index) => (
+        <li
+          key={airport.iata}
+          className={`list-group-item list-group-item-action`}
+          onClick={() => {
+            selectItem(airport);
+          }}
+        >
+          <strong>{airport.iata}</strong> - {airport.name}, {airport.city},{" "}
+          {airport.country}
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <div className="form-group">
@@ -39,11 +72,19 @@ export const AirportSelect = () => {
         className="form-control"
         id="origin"
         type="text"
+        placeholder={
+          selected
+            ? `${selected.iata} - ${selected.name}, ${selected.city}, ${
+                selected.country
+              }`
+            : ""
+        }
         value={searchText}
         onChange={e => {
           doSearch(e.currentTarget.value);
         }}
       />
+      {suggestions ? <SuggestionList /> : ""}
     </div>
   );
 };
