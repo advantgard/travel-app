c002963cfe91f935a6bdde1266a2b4c2c7ea289c
Jorge Alvarez <j.alvarz@hotmail.com>
2019-11-05 11:22:40 -0800

    Fix firebase warning

--- services/firebase.js
+++ services/firebase.js
@@ -1,19 +1,20 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBy0vm8D-sxttWkryDAHrO7K1KgWxYv_Io",
-    authDomain: "travel-planner-e252f.firebaseapp.com",
-    databaseURL: "https://travel-planner-e252f.firebaseio.com",
-    projectId: "travel-planner-e252f",
-    storageBucket: "travel-planner-e252f.appspot.com",
-    messagingSenderId: "732037472555",
-    appId: "1:732037472555:web:c7c658aee75003547ba56e",
-    measurementId: "G-N5JHW91CBX"
+  apiKey: "AIzaSyBy0vm8D-sxttWkryDAHrO7K1KgWxYv_Io",
+  authDomain: "travel-planner-e252f.firebaseapp.com",
+  databaseURL: "https://travel-planner-e252f.firebaseio.com",
+  projectId: "travel-planner-e252f",
+  storageBucket: "travel-planner-e252f.appspot.com",
+  messagingSenderId: "732037472555",
+  appId: "1:732037472555:web:c7c658aee75003547ba56e",
+  measurementId: "G-N5JHW91CBX"
 };
 
-if(!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase;
